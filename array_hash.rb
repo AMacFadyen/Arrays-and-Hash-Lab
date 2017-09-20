186e8c0fa7f859e7966253697dcc8e3915a00814
AMacFadyen <ferretsbite@hotmail.co.uk>
2017-09-20 16:11:07 +0100

    initial Array Hash Lab attempt

---
+++ array_hash.rb
@@ -0,0 +1,22 @@
+
+
+def comb_array_length(arr1, arr2)
+  return arr1.length() + arr2.length()
+end
+
+def array_sum(array)
+  return array.sum
+end
+
+def find_item(array, house_name)
+  for house in array
+    if house == house_name
+      return true
+    end
+  end
+  return false
+end
+
+def teacher_wallets(key)
+  return hash.keys[key]
+end
---
+++ array_hash_spec.rb
@@ -0,0 +1,42 @@
+require ('minitest/autorun')
+require ('minitest/rg')
+require_relative('./array_hash')
+
+class ArrayHashPractise < MiniTest::Test
+
+
+  def test_comb_array_length()
+    prices = [ 1.23, 6.98, 8.43, 2.45]
+    costs = [ 4.23, 1.12, 0.52, 8.67]
+    result = comb_array_length(prices, costs)
+    assert_equal(8, result)
+  end
+
+def test_array_sum
+  data = [1, 2, 3, 4, 5]
+  restult = array_sum(data)
+  assert_equal(15, restult)
+end
+
+def test_find_item
+  hogwarts = ["slytherin", "hufflepuff", "gryfindor", "ravenclaw"]
+  result = find_item(hogwarts, "ravenclaw")
+  result2 = find_item(hogwarts, "Batman")
+  assert_equal(true, result)
+  assert_equal(false, result2)
+end
+
+def test_teacher_wallets
+  teacher_wallets = {
+    Sandy: 12,
+    Zsolt: 10,
+    Val: 1356,
+    Jay: 1
+  }
+result = teacher_wallets.key[1]
+  assert_equal(:Sandy, result)
+end
+
+
+end
+#class end
